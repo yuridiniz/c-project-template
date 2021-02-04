3a9e905045fccbe5f304d81d13e5f5bf6794c014
Yuri Araújo <yuri.araujod@gmail.com>
2021-02-03 23:47:16 -0300

    Refactor

--- src/main.c
+++ src/main.c
@@ -15,42 +15,17 @@ void start_command(int argc, char *argv[], int index);
 void stop_command(int argc, char *argv[], int index);
 
 static options_t options[] = {
-    {start_command, "start", "start", "Iniciar o serviço"},
-    {stop_command, "stop", "stop", "Para o serviço"},
-    {show_help_command, "-h", "--help", "Exibe os detalhes das operações"},
-    {show_help_command, "", "", ""},
+    {start_command,         "start",    "start", "  Iniciar o serviço"},
+    {stop_command,          "stop",     "stop",     "Para o serviço"},
+    {show_help_command,     "-h",       "--help",   "Exibe os detalhes das operações"},
+    {show_help_command,     "",         "",         ""},
 };
-
-void stop_command(int argc, char *argv[], int index) {
-    char * pid_file = "/var/run/mqttd/mqttd.pid";
-    int pid = get_pid(pid_file);
-
-    if(access(pid_file, F_OK) != 0 && errno == EACCES) {
-        printf("[ERROR] Não foi possível parar o processo de pid %d devido ao mesmo ter sido iniciado por outro usuário\n", pid);
-        exit(EXIT_FAILURE);
-    }
-
-    if(!daemon_is_running(pid)) {
-        printf("[WAN] Processo não está em execução\n");
-        exit(EXIT_FAILURE);
-    }
-
-    remove(pid_file);
-
-    send_daemon_signal(pid, SIGKILL);
-    exit(0);
-}
 
 void sigkill_handler(int _) {
     exit(0);
 }
 
-void start_command(int argc, char *argv[], int index) {
-    char * pid_file = "/var/run/mqttd/mqttd.pid";
-
-    daemon_signal_t config;
-    config.sigkill_handler = sigkill_handler;
-
+void check_deamon_to_start(char * pid_file) {
     int pid = get_pid(pid_file);
 
     if(access(pid_file, W_OK) != 0 && errno == EACCES) {
@@ -62,6 +37,42 @@ void start_command(int argc, char *argv[], int index) {
         printf("[WARN] Processo já está em execução com pid %d\n", pid);
         exit(EXIT_FAILURE);
     }
+}
+
+int check_daemon_to_stop(char * pid_file) 
+{
+    int pid = get_pid(pid_file);
+
+    if(access(pid_file, F_OK) != 0 && errno == EACCES) {
+        printf("[ERROR] Não foi possível parar o processo de pid %d devido ao mesmo ter sido iniciado por outro usuário\n", pid);
+        exit(EXIT_FAILURE);
+    }
+
+    if(!daemon_is_running(pid)) {
+        printf("[WAN] Processo não está em execução\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    return pid;
+}
+
+void stop_command(int argc, char *argv[], int index) {
+    char * pid_file = "/var/run/mqttd/mqttd.pid";
+    
+    int pid = check_daemon_to_stop(pid_file);
+    send_daemon_signal(pid, SIGKILL);
+    
+    remove(pid_file);
+    exit(0);
+}
+
+void start_command(int argc, char *argv[], int index) {
+    char * pid_file = "/var/run/mqttd/mqttd.pid";
+
+    check_deamon_to_start(pid_file);
+    
+    daemon_signal_t config;
+    config.sigkill_handler = sigkill_handler;
 
     daemonize(&config);
 
